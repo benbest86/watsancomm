477acc121db7fdc2f9b1d07ac208903d5ea16072
Ben Best <ben.best@gmail.com>
2011-04-06 23:13:23 +0000

    Captures received emails in the data store.

--- handle_weekly.py
+++ handle_weekly.py
@@ -2,10 +2,29 @@ import email, logging
 from google.appengine.ext import webapp 
 from google.appengine.ext.webapp.mail_handlers import InboundMailHandler 
 from google.appengine.ext.webapp.util import run_wsgi_app
+from models import WeeklyUpdate
 
 class WeeklyUpdateHandler(InboundMailHandler):
     def receive(self, mail_message):
-        logging.info("Received a message from: " + mail_message.sender)
-        print 'Received a message from %s' % mail_message.sender
+        sender = mail_message.sender
+        for content_type, body in mail_message.bodies('text/plain'):
+            plain_body = body.decode()
+        for content_type, body in mail_message.bodies('text/html'):
+            html_body = body.decode()
+        message_date = mail_message.date
+        wu = WeeklyUpdate(
+                sender=sender,
+                plain_body=plain_body,
+                html_body=html_body,
+                )
+        wu.put()
 
 application = webapp.WSGIApplication([WeeklyUpdateHandler.mapping()], debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == '__main__':
+    main()
+
+
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 import re
-from google.appengine.ext import db
+from google.appengine.ext import db, webapp
+from google.appengine.api.mail import EmailMessage
 
 
 class ParseError(Exception):
@@ -10,7 +11,7 @@ class WeeklyUpdate(db.Model):
     sender = db.StringProperty(required=True)
     html_body = db.TextProperty()
     plain_body = db.TextProperty()
-    date = db.DateProperty(required=True)
+    datetime_received_at = db.DateTimeProperty(auto_now_add=True)
 
     def parse(self):
         body = self.plain_body or self.html_body
