f93e2e583357d1ce57b0229d3070af8aa68940f8
Ben Best <ben.best@gmail.com>
2011-04-08 15:43:32 +0000

    removing unncessary TEMPLATE_LOADERS variable from settings.

--- settings.py
+++ settings.py
@@ -1,7 +1,4 @@
 import os
 ROOT_PATH = os.path.join(os.path.dirname(__file__))
 # List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-)
 TEMPLATE_DIR = os.path.join(ROOT_PATH, 'templates')
