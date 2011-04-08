487e881e683ce0e893d007d63a0d038be041ef8b
Ben Best <ben.best@gmail.com>
2011-04-08 15:41:27 +0000

    changing preview weekly view to use the exact content generated for the html email for a more realistic test.

--- watsancomm.py
+++ watsancomm.py
@@ -3,17 +3,15 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 from google.appengine.dist import use_library
 use_library('django', '1.2')
 from google.appengine.ext import webapp
-from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-from settings import TEMPLATE_DIR
 from models import WeeklyUpdate
 
 class PreviewWeekly(webapp.RequestHandler):
     def get(self):
         content = WeeklyUpdate.generate_summary_content([])
-        path = os.path.join(TEMPLATE_DIR, 'html_mail.html')
-        self.response.out.write(template.render(path, content))
+        email = WeeklyUpdate.generate_summary_email(content)
+        self.response.out.write(email.html)
 
 application = webapp.WSGIApplication(
                                      [('/', PreviewWeekly)],
