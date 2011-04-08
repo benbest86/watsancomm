c9486a863b6ac244e8da4cdfadb1603d8bb813c5
Ben Best <ben.best@gmail.com>
2011-04-08 15:51:07 +0000

    adding code to use django 1.2 to weekly_handler in case it gets used first.

--- handle_weekly.py
+++ handle_weekly.py
@@ -1,4 +1,7 @@
-import email, logging
+import email, logging, os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+from google.appengine.dist import use_library
+use_library('django', '1.2')
 from google.appengine.ext import webapp 
 from google.appengine.ext.webapp.mail_handlers import InboundMailHandler 
 from google.appengine.ext.webapp.util import run_wsgi_app
