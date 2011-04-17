659cb04f957a9dcd1201bdae00857da30d8f9d97
Ben Best <ben.best@gmail.com>
2011-04-17 22:05:14 +0000

    Disregarding all mail to weeklysummary@...

---
+++ disregard_mail.py
@@ -0,0 +1,22 @@
+import email, logging, os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+from google.appengine.dist import use_library
+use_library('django', '1.2')
+from google.appengine.ext import webapp 
+from google.appengine.ext.webapp.mail_handlers import InboundMailHandler 
+from google.appengine.ext.webapp.util import run_wsgi_app
+from models import WeeklyUpdate, ValidationError
+
+class DisregardHandler(InboundMailHandler):
+    def receive(self, mail_message):
+        pass
+
+application = webapp.WSGIApplication([DisregardHandler.mapping()], debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == '__main__':
+    main()
+
+
